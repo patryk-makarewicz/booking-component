3500e269c55dec068f57939fe8f785f0e2eb9e93
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-02-16 14:56:29 +0100

    feat: button ready done

---
+++ src/components/Buttons/Ready.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  outline-style: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 169px;
+  height: 46px;
+  background: #1ba7e0;
+  border: 1px solid #2193c1;
+  box-shadow: 0px 1px 8px rgba(0, 0, 0, 0.15);
+  border-radius: 2px;
+
+  &:hover {
+    background: rgba(27, 167, 224, 0.8);
+    border: 1px solid rgba(33, 147, 193, 0.8);
+  }
+
+  &:active {
+    background: #1ba7e0;
+    border: 1px solid #2193c1;
+    box-shadow: 0px 1px 8px rgba(0, 0, 0, 0.15);
+  }
+`;
+
+const Children = styled.p`
+  color: #ffffff;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 21px;
+`;
+
+const ButtonReady = ({ children }) => (
+  <Button type="submit">
+    <Children>{children}</Children>
+  </Button>
+);
+
+export default ButtonReady;
--- src/views/App.js
+++ src/views/App.js
@@ -3,14 +3,16 @@ import React from 'react';
 import GlobalStyle from 'theme/GlobalStyle';
 
 import './App.scss';
-import Button from '../components/Buttons/Submit';
+import ButtonSubmit from '../components/Buttons/Submit';
+import ButtonReady from '../components/Buttons/Ready';
 
 const App = () => (
   <>
     <GlobalStyle />
     <div className="app">
       <header className="app__header">Booking app</header>
-      <Button>Submit</Button>
+      <ButtonSubmit>Submit</ButtonSubmit>
+      <ButtonReady>Ready</ButtonReady>
     </div>
   </>
 );
