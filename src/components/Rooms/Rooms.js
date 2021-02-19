e9141775f1040fb4886f697ab8bfcbf7b6f7071b
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-02-19 15:47:06 +0100

    feat: connectrooms with redux,show box button add

--- src/components/Rooms/Rooms.js
+++ src/components/Rooms/Rooms.js
@@ -1,23 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { toggleShowBox } from 'redux/Actions/ui-actions';
 
 import styles from './Room.module.scss';
-
 import BedIcon from '../../assets/bed.svg';
 import PersonIcon from '../../assets/person.svg';
 import ButtonArrowNoBorder from '../Buttons/ArrowNoBorder';
+import Box from '../Box/Box';
 
-const Rooms = () => (
-  <div className={styles.room}>
-    <div className={styles.room__bed}>
-      <img className={styles.room__icon} src={BedIcon} alt="Bed icon" />
-      <p className={styles.room__number}>0</p>
-    </div>
-    <div className={styles.room__person}>
-      <img className={styles.room__icon} src={PersonIcon} alt="Person icon" />
-      <p className={styles.room__number}>0</p>
-      <ButtonArrowNoBorder />
-    </div>
-  </div>
-);
+const Rooms = (props) => {
+  const { openBox, showBox } = props;
 
-export default Rooms;
+  return (
+    <>
+      <div className={styles.room}>
+        <div className={styles.room__bed}>
+          <img className={styles.room__icon} src={BedIcon} alt="Bed icon" />
+          <p className={styles.room__number}>0</p>
+        </div>
+        <div className={styles.room__person}>
+          <img className={styles.room__icon} src={PersonIcon} alt="Person icon" />
+          <p className={styles.room__number}>0</p>
+          <ButtonArrowNoBorder onPress={() => openBox()} />
+        </div>
+      </div>
+      {showBox && <Box />}
+    </>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  openBox: () => dispatch(toggleShowBox()),
+});
+
+const mapStateToProps = (state) => ({
+  showBox: state.UI.showBox,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Rooms);
--- src/views/StartView/index.js
+++ src/views/StartView/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 import Rooms from '../../components/Rooms/Rooms';
-import Box from '../../components/Box/Box';
 
 const StartView = () => (
   <>
     <Rooms />
-    <Box />
   </>
 );
 
