c220573bbe777f8def5583574725908fcb6d9675
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-02-19 22:31:46 +0100

    feat: add number of rooms

--- src/components/Rooms/Rooms.js
+++ src/components/Rooms/Rooms.js
@@ -10,18 +10,31 @@ import ButtonArrowNoBorder from '../Buttons/ArrowNoBorder';
 import Box from '../Box/Box';
 
 const Rooms = (props) => {
-  const { openBox, showBox } = props;
+  const {
+    openBox,
+    showBox,
+    adultsNumber,
+    childrenNumber,
+    adultsTwoNumber,
+    childrenTwoNumber,
+  } = props;
+
+  const sum = adultsNumber + childrenNumber + adultsTwoNumber + childrenTwoNumber;
 
   return (
     <>
       <div className={styles.room}>
         <div className={styles.room__bed}>
           <img className={styles.room__icon} src={BedIcon} alt="Bed icon" />
-          <p className={styles.room__number}>0</p>
+          {adultsTwoNumber >= 1 ? (
+            <p className={styles.room__number}>2</p>
+          ) : (
+            <p className={styles.room__number}> {adultsNumber >= 1 ? 1 : 0}</p>
+          )}
         </div>
         <div className={styles.room__person}>
           <img className={styles.room__icon} src={PersonIcon} alt="Person icon" />
-          <p className={styles.room__number}>0</p>
+          <p className={styles.room__number}>{sum}</p>
           <ButtonArrowNoBorder onPress={() => openBox()} showBox={showBox} />
         </div>
       </div>
@@ -36,6 +49,10 @@ const mapDispatchToProps = (dispatch) => ({
 
 const mapStateToProps = (state) => ({
   showBox: state.UI.showBox,
+  adultsNumber: state.roomOne.adults,
+  childrenNumber: state.roomOne.children,
+  adultsTwoNumber: state.roomTwo.adultsTwo,
+  childrenTwoNumber: state.roomTwo.childrenTwo,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Rooms);
