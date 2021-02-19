f738c6a5ba5328465d4ec80e612f2e580093065b
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-02-19 10:08:40 +0100

    feat: actions, types, reducer for inc and dec

---
+++ src/redux/Counter/reducer.js
@@ -0,0 +1,23 @@
+import * as actionTypes from './actions';
+
+const INITIAL_STATE = {
+  room: 0,
+  person: 0,
+};
+
+const counterReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case actionTypes.COUNTER_ROOM_INC:
+      return { ...state, room: state.room + 1 };
+    case actionTypes.COUNTER_ROOM_DEC:
+      return { ...state, room: state.room - 1 };
+    case actionTypes.COUNTER_PERSON_INC:
+      return { ...state, person: state.person + 1 };
+    case actionTypes.COUNTER_PERSON_DEC:
+      return { ...state, person: state.person - 1 };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
